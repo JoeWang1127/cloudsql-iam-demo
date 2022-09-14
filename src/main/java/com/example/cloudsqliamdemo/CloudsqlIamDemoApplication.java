51fdd7895dea212fb73ab6b0d99f6465ad7e857e
Joe Wang <joewa@google.com>
2022-09-14 11:34:10 -0400

    init commit

---
+++ src/main/java/com/example/cloudsqliamdemo/CloudsqlIamDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudsqliamdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudsqlIamDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CloudsqlIamDemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/cloudsqliamdemo/CloudsqlIamDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudsqliamdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudsqlIamDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
